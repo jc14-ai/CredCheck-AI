64937bebcb9c9bdc5fdf76175f4ae156355f0bec
Jzy <jestalycastillo15@gmail.com>
2025-07-30 16:10:09 +0800

    Frontend setups

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 import flask
 import joblib
+
+def load_model():
+    model = joblib.load('Training/model.pkl')
+    
+    print("model returned!")
+    return model
+
+if __name__ == '__main__':
+    model = load_model()
+    print('Ready!')
