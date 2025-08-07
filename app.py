d790d519f76a97d7ad28cb1d89ae84f168970645
Jzy <jestalycastillo15@gmail.com>
2025-08-07 16:41:59 +0800

    added page routing

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ model = joblib.load('Training/model.pkl')
 def home():
     return render_template('index.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/detector')
+def detector():
+    return render_template('detector.html')
+
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
